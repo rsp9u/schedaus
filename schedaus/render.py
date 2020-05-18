8a2cc9791a7df6d5f0bb96c836fdfdfe0a8e612c
rsp9u <gctwt852@yahoo.co.jp>
2020-05-19 00:21:34 +0900

    support weekly scale

--- schedaus/render.py
+++ schedaus/render.py
@@ -6,9 +6,6 @@ from schedaus.model import Task
 from schedaus.utils import get_prefix_space_num, len_multibyte, calc_remain_days_in_month
 
 
-w = 16
-h = 16
-
 text_common_opts = {
     "font_family": "Serif",
     "dominant_baseline": "hanging",
@@ -28,10 +25,15 @@ class Renderer:
 
         # width per day
         self.wpd = 8
+        # height per line
+        self.hpl = 16
         # maximum width
         self.mw = -1
         # maximum height
         self.mh = -1
+
+        self.scale = "daily"
+        self.schedule_offset = self.hpl * 4
 
     def get_svg(self):
         return self.dwg
@@ -80,6 +82,8 @@ class Renderer:
         schedules_y = {}
         idx = 0
 
+        self.change_scale(data["calendar"].scale)
+
         objs = self.calendar_to_svg(data["calendar"], data)
 
         def _find(items, name):
@@ -88,7 +92,7 @@ class Renderer:
         for group in data["groups"]:
             if group.text:
                 group_objs = self.group_to_svg(data["calendar"], group)
-                y = h * 2 * (idx + 2)
+                y = self.schedule_offset + self.hpl * (idx * 2)
                 g = self.dwg.g(id=f"group-{group.text}", transform=f"translate(0, {y})")
                 for group_obj in group_objs:
                     g.add(group_obj)
@@ -96,7 +100,7 @@ class Renderer:
                 idx += 1
             for sc_name in group.member:
                 sc = _find(data["schedules"], sc_name)
-                y = h * 2 * (idx + 2)
+                y = self.schedule_offset + self.hpl * (idx * 2)
                 g = self._render_schedule(sc, y, data)
                 objs.append({"z": 20, "o": g})
                 schedules_y[sc.name] = y
@@ -110,10 +114,14 @@ class Renderer:
         for obj in sorted(objs, key=lambda x: x["z"]):
             self.dwg.add(obj["o"])
 
-        mw = w * ((data["calendar"].end - data["calendar"].start).days + 1)
-        mh = (h * 3) + (h * 2 * (len(data["schedules"]) + len(data["groups"])))
-        self.dwg.attribs["width"] = f"{mw}px"
-        self.dwg.attribs["height"] = f"{mh}px"
+    def change_scale(self, scale):
+        self.scale = scale
+        if scale == "daily":
+            self.wpd = 16
+            self.schedule_offset = self.hpl * 4
+        elif scale == "weekly":
+            self.wpd = 8
+            self.schedule_offset = self.hpl * 3
 
     def _render_schedule(self, schedule, y, data):
         if isinstance(schedule, Task):
@@ -133,8 +141,10 @@ class Renderer:
 
     def calendar_to_svg(self, calendar, data):
         ph = 1
-        mw = w * ((calendar.end - calendar.start).days + 1)
-        mh = (h * 3) + (h * 2 * (len(data["schedules"]) + len(data["groups"])))
+        self.mw = self.wpd * ((calendar.end - calendar.start).days + 1)
+        self.mh = (self.hpl * 3) + (self.hpl * 2 * (len(data["schedules"]) + len(data["groups"])))
+        self.dwg.attribs["width"] = f"{self.mw}px"
+        self.dwg.attribs["height"] = f"{self.mh}px"
 
         objs = []
         prev_month = None
@@ -152,31 +162,57 @@ class Renderer:
         date = calendar.start
         while date <= calendar.end:
             if date.month != prev_month:
+                # draw 'YYYY/MM' text
                 if calc_remain_days_in_month(date) >= 3:
                     month_text = "{}/{}".format(date.year, date.month)
                 else:
                     month_text = "{}".format(date.month)
                 objs.append({"z": 5, "o": self.dwg.text(month_text, (x+1, ph), **text_month_opts)})
-                objs.append({"z": 10, "o": self.dwg.line((x, 0), (x, mh), **line_common_opts)})
+                # draw line per month
+                if self.scale == "daily":
+                    h = self.mh
+                elif self.scale == "weekly":
+                    h = self.hpl
+                objs.append({"z": 10, "o": self.dwg.line((x, 0), (x, h), **line_common_opts)})
+                # update previous month
                 prev_month = date.month
-            objs.append({"z": 5, "o": self.dwg.text(date.strftime("%a")[0:2], (x+w/2, h+ph), **text_day_opts)})
-            objs.append({"z": 5, "o": self.dwg.text(date.day, (x+w/2, h*2+ph), **text_day_opts)})
-            objs.append({"z": 10, "o": self.dwg.line((x, h*3), (x, mh), **line_common_opts)})
-            x += w
+
+            if self.scale == "daily":
+                # draw weekday text
+                objs.append({"z": 5, "o": self.dwg.text(date.strftime("%a")[0:2], (x+self.wpd/2, self.hpl+ph), **text_day_opts)})
+                # draw day text
+                objs.append({"z": 5, "o": self.dwg.text(date.day, (x+self.wpd/2, self.hpl*2+ph), **text_day_opts)})
+                # draw line per day
+                objs.append({"z": 10, "o": self.dwg.line((x, self.hpl*3), (x, self.mh), **line_common_opts)})
+            elif self.scale == "weekly":
+                if date.weekday() == 0:
+                    # draw day text
+                    objs.append({"z": 5, "o": self.dwg.text(date.day, (x+self.wpd/2+2, self.hpl+ph), **text_day_opts)})
+                    # draw line per week
+                    objs.append({"z": 10, "o": self.dwg.line((x, self.hpl), (x, self.mh), **line_common_opts)})
+            x += self.wpd
             date += timedelta(days=1)
+
         for closed in calendar.closed:
             offset_days = (closed - calendar.start).days
-            xy = (w * offset_days, h)
-            objs.append({"z": 0, "o": self.dwg.rect(xy, (w, mh-h), fill="#D0D0D0")})
+            if self.scale == "daily":
+                xy = (self.wpd * offset_days, self.hpl)
+            elif self.scale == "weekly":
+                xy = (self.wpd * offset_days, self.hpl*2)
+            objs.append({"z": 0, "o": self.dwg.rect(xy, (self.wpd, self.mh-self.hpl), fill="#D0D0D0")})
 
         # Outer frame
-        objs.append({"z": 10, "o": self.dwg.line((mw, 0), (mw, mh), **line_common_opts)})
-        objs.append({"z": 10, "o": self.dwg.line((0, h*0), (mw, h*0), **line_common_opts)})
-        objs.append({"z": 10, "o": self.dwg.line((0, h*3), (mw, h*3), **line_common_opts)})
+        objs.append({"z": 10, "o": self.dwg.line((self.mw, 0), (self.mw, self.mh), **line_common_opts)})
+        objs.append({"z": 10, "o": self.dwg.line((0, self.hpl*0), (self.mw, self.hpl*0), **line_common_opts)})
+        if self.scale == "daily":
+            objs.append({"z": 10, "o": self.dwg.line((0, self.hpl*3), (self.mw, self.hpl*3), **line_common_opts)})
+        elif self.scale == "weekly":
+            objs.append({"z": 10, "o": self.dwg.line((0, self.hpl*1), (self.mw, self.hpl*1), **line_common_opts)})
+            objs.append({"z": 10, "o": self.dwg.line((0, self.hpl*2), (self.mw, self.hpl*2), **line_common_opts)})
 
         # Today's line
-        x = w * (calendar.today - calendar.start).days
-        objs.append({"z": 99, "o": self.dwg.line((x, 0), (x, mh), stroke="#FF0000", stroke_width="3.0")})
+        x = self.wpd * (calendar.today - calendar.start).days
+        objs.append({"z": 99, "o": self.dwg.line((x, 0), (x, self.mh), stroke="#FF0000", stroke_width="3.0")})
 
         return objs
 
@@ -185,8 +221,8 @@ class Renderer:
         delta_to_end = task.plan_end - calendar.start + timedelta(days=1)
 
         objs = []
-        xy = (w*delta_to_start.days, 0)
-        wh = (w*(delta_to_end - delta_to_start).days, h)
+        xy = (self.wpd*delta_to_start.days, 0)
+        wh = (self.wpd*(delta_to_end - delta_to_start).days, self.hpl)
         rect = self.dwg.rect(xy, wh, rx=6, ry=6, fill=task.color_plan_fill, stroke=task.color_plan_outline)
         objs.append({"z": 20, "o": rect})
         text_task_opts = {"font_size": "13", "color": task.color_text}
@@ -200,8 +236,8 @@ class Renderer:
             delta_to_start = task.actual_start - calendar.start
             delta_to_end = task.actual_end - calendar.start + timedelta(days=1)
 
-            xy = (w*delta_to_start.days, h+2)
-            wh = (w*(delta_to_end - delta_to_start).days, 4)
+            xy = (self.wpd*delta_to_start.days, self.hpl+2)
+            wh = (self.wpd*(delta_to_end - delta_to_start).days, 4)
             if task.actual_completed is not None:
                 opt = {"fill": "#FFFFFF", "stroke_dasharray": "4 3"}
             else:
@@ -210,7 +246,7 @@ class Renderer:
             objs.append({"z": 20, "o": rect})
 
             if task.actual_progress is not None:
-                xy = (w*delta_to_end.days+2, h+5)
+                xy = (self.wpd*delta_to_end.days+2, self.hpl+5)
                 text_opts = {
                     "font_family": "Serif",
                     "font_size": 10,
@@ -222,20 +258,21 @@ class Renderer:
 
             if task.actual_completed is not None:
                 delta_to_completed = task.actual_completed - calendar.start
-                xy = (w*delta_to_start.days, h+2)
-                wh = (w*(delta_to_completed - delta_to_start).days, 4)
+                xy = (self.wpd*delta_to_start.days, self.hpl+2)
+                wh = (self.wpd*(delta_to_completed - delta_to_start).days, 4)
                 rect = self.dwg.rect(xy, wh, rx=2, ry=2, fill=task.color_actual_fill, stroke=task.color_actual_outline)
                 objs.append({"z": 21, "o": rect})
 
         return objs
 
     def milestone_to_svg(self, calendar, milestone):
-        x = w * (milestone.plan_happen - calendar.start).days
-        path = [(w/2, 0), (0, h/2), (w/2, h), (w, h/2), (w/2, 0)]
+        x = self.wpd * ((milestone.plan_happen - calendar.start).days + 0.5)
+        s = self.hpl / 2
+        path = [(0, 0), (-s, s), (0, s*2), (s, s), (0, 0)]
         m = self.dwg.polyline(path, fill=milestone.color_plan_fill, stroke=milestone.color_plan_outline)
         text_ms_opts = {"font_size": "13", "color": milestone.color_text}
         text_ms_opts.update(text_common_opts)
-        t = self.dwg.text(milestone.text, (w+2, 2), **text_ms_opts)
+        t = self.dwg.text(milestone.text, (self.hpl/2+2, 2), **text_ms_opts)
         g = self.dwg.g(transform=f"translate({x}, 0)")
         g.add(m)
         g.add(t)
@@ -265,24 +302,31 @@ class Renderer:
                 direction = ["left", "up"]
 
         if direction[0] == "down":
-            sx = w * (dpath.start_date - calendar.start).days + w / 2
-            sy = schedules_y[dpath.start_name] + h
+            sx = self.wpd * (dpath.start_date - calendar.start).days + self.wpd / 2
+            sy = schedules_y[dpath.start_name] + self.hpl
         else:
             if direction[0] == "right":
-                sx = w * (dpath.start_date - calendar.start).days + w
+                sx = self.wpd * (dpath.start_date - calendar.start).days + self.wpd
             else:
-                sx = w * (dpath.start_date - calendar.start).days
-            sy = schedules_y[dpath.start_name] + h / 2
+                sx = self.wpd * (dpath.start_date - calendar.start).days
+            sy = schedules_y[dpath.start_name] + self.hpl / 2
 
         if direction[0] != "down":
-            ex = w * (dpath.end_date - calendar.start).days + w / 2
-            ey = schedules_y[dpath.end_name] + h
+            ex = self.wpd * (dpath.end_date - calendar.start).days + self.wpd / 2
+            ey = schedules_y[dpath.end_name] + self.hpl
         else:
             if direction[1] == "right":
-                ex = w * (dpath.end_date - calendar.start).days
+                ex = self.wpd * (dpath.end_date - calendar.start).days
             else:
-                ex = w * (dpath.end_date - calendar.start).days + w
-            ey = schedules_y[dpath.end_name] + h / 2
+                ex = self.wpd * (dpath.end_date - calendar.start).days + self.wpd
+            ey = schedules_y[dpath.end_name] + self.hpl / 2
+
+        # adjust for weekly scale
+        if self.scale == "weekly":
+            if direction[1] == "right":
+                sx -= self.wpd * 0.5
+            if direction[1] == "left":
+                sx += self.wpd * 0.5
 
         if direction[0] == "down":
             mx = sx
@@ -305,15 +349,14 @@ class Renderer:
         return g
 
     def group_to_svg(self, calendar, group):
-        mw = w * ((calendar.end - calendar.start).days + 1)
         objs = []
         text_size = len_multibyte(group.text) * 8 + 10
         text_size = len(group.text) * 8 + 10
         line_opts = {"style": "stroke:dimgray; stroke-width=1.0"}
         text_opts = {"font_family": "Serif", "font_size": 13, "dominant_baseline": "middle", "text_anchor": "middle"}
-        objs.append(self.dwg.line((0, h/5*2), (mw/2 - text_size/2, h/5*2), **line_opts))
-        objs.append(self.dwg.line((0, h/5*3), (mw/2 - text_size/2, h/5*3), **line_opts))
-        objs.append(self.dwg.text(group.text, (mw/2, h/2), **text_opts))
-        objs.append(self.dwg.line((mw/2 + text_size/2, h/5*2), (mw, h/5*2), **line_opts))
-        objs.append(self.dwg.line((mw/2 + text_size/2, h/5*3), (mw, h/5*3), **line_opts))
+        objs.append(self.dwg.line((0, self.hpl/5*2), (self.mw/2 - text_size/2, self.hpl/5*2), **line_opts))
+        objs.append(self.dwg.line((0, self.hpl/5*3), (self.mw/2 - text_size/2, self.hpl/5*3), **line_opts))
+        objs.append(self.dwg.text(group.text, (self.mw/2, self.hpl/2), **text_opts))
+        objs.append(self.dwg.line((self.mw/2 + text_size/2, self.hpl/5*2), (self.mw, self.hpl/5*2), **line_opts))
+        objs.append(self.dwg.line((self.mw/2 + text_size/2, self.hpl/5*3), (self.mw, self.hpl/5*3), **line_opts))
         return objs
